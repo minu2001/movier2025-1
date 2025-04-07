ccafb287dcb14ae587f04f1cf55c2c8bf4d7111b
minu2001 <alsdn3015@g.shingu.ac.kr>
2025-04-07 15:49:03 +0900

    영화추천 중간 저장.

--- front/movie_js.js
+++ front/movie_js.js
@@ -13,11 +13,12 @@ let MovieObject = {
 
             console.log(response)
             movielist =  response.result
-
-            topdiv = document.createElement("div")
-            topdiv.style = "column-count:5"
-            document.body.appendChild(topdiv)
-
+            
+            //topdiv = document.createElement("div")
+            //topdiv.style = "column-count:5"
+            //document.body.appendChild(topdiv)
+            
+            topdiv = document.getElementById('alldiv')
             movielist.forEach(movie => {
                 cmovie = document.createElement("div")
                 cmovie.className = "card"
@@ -41,7 +42,46 @@ let MovieObject = {
         // 실패코드
             console.log(error)
         });
+    },
+
+
+    getgenres: function() {
+        sgenre = document.getElementById("sgenre").value;  // select에서 선택된 장르 가져오기
+    
+        $.ajax({
+            type: "GET",
+            url: `http://localhost:8000/genresbest/${sgenre}`,  // ✅ 템플릿 리터럴로 동적 URL 생성
+        }).done(function(response) {
+            console.log(response);
+            movielist = response.result;
+    
+            topdiv = document.getElementById('genrediv');
+            // topdiv.innerHTML = "";  // 이전 내용 제거 첫번 째 방법
+            while(topdiv.firstChild){
+                topdiv.removeChild(topdiv.firstChild)
+            }
+            movielist.forEach(movie => {
+                const cmovie = document.createElement("div");
+                cmovie.className = "card";
+    
+                const mimg = document.createElement("img");
+                mimg.className = "card-img-top";
+                mimg.src = movie.poster_path;
+                mimg.onclick = function() {
+                    window.open(movie.url);  // 새창에서 열기
+                };
+                mimg.style.cursor = "pointer";
+    
+                cmovie.appendChild(mimg);
+                topdiv.appendChild(cmovie);
+            });
+    
+        }).fail(function(error) {
+            console.log(error);
+        });
     }
+    
+    
 }
 
 MovieObject.init();
